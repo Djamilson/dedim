e5c9cddae605fe9621dbd69dd4e32b8099199b9f
Djamilson <djamilson@gmail.com>
2020-10-21 21:38:26 -0300

    Create init

--- src/components/Headers/Menu/index.tsx
+++ src/components/Headers/Menu/index.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
+
+import { motion } from 'framer-motion';
 
 import { Container, MenuContainer } from './styles';
 
 const Menu: React.FC = () => {
+  const scrollPosition = window.pageYOffset;
+
+  // If back to element (use Ref)
+  const [s, setS] = useState(0);
+  const [ss, setSs] = useState(s);
+
+  function onClick() {
+    alert('click');
+    console.log('mmm', window.pageYOffset);
+    setS(s + 150);
+  }
+
+  useEffect(() => {
+    setSs(s);
+  }, [s]);
+
   return (
     <Container>
       <MenuContainer>
         <ul>
           <li>
             <Link to="/">
-              <span>Home</span>
+              <span>Início</span>
             </Link>
           </li>
           <li>
             <Link to="/">
-              <span> Projetos</span>
+              <span> Perfil</span>
             </Link>
           </li>
           <li>
